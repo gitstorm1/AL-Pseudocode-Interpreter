479d2e5b1f270d9bdd6e207b29a38dfefd2ab30f
Abdul Hadi <56407595+gitstorm1@users.noreply.github.com>
2023-08-12 15:47:27 +0500

    Added more tokens in token.py. Still in progress.

--- interpreter/token.py
+++ interpreter/token.py
@@ -1,31 +1,100 @@
 from enum import Enum
 
 class TokenTypes(Enum):
+    ##### Special tokens
     ILLEGAL = 'ILLEGAL'
     EOF = ''
     EOL = '\n'
     
+    ##### For function and variable names
     IDENTIFIER = 'IDENTIFIER'
     
+    ##### Native datatypes
     INTEGER = 'INTEGER'
     REAL = 'REAL'
+    CHAR = 'CHAR'
+    STRING = 'STRING'
+    BOOLEAN = 'BOOLEAN'
+    DATE = 'DATE'
     
-    # Operators
+    ##### Keywords
+    # WHILE loop
+    WHILE = 'WHILE'
+    ENDWHILE = 'ENDWHILE'
+    
+    # REPEAT UNTIL loop
+    REPEAT = 'REPEAT'
+    UNTIL = 'UNTIL'
+    
+    # FOR loop
+    FOR = 'FOR'
+    TO = 'TO'
+    STEP = 'STEP'
+    NEXT = 'NEXT'
+    
+    # Declaring of variables and constants
+    DECLARE = 'DECLARE'
+    CONSTANT = 'CONSTANT'
+    
+    # Declaring of arrays
+    ARRAY = 'ARRAY'
+    OF = 'OF'
+    
+    # Declaring of user-defined types, including pointers
+    TYPE = 'TYPE'
+    ENDTYPE = 'ENDTYPE'
+    
+    # Console input and output
+    INPUT = 'INPUT'
+    OUTPUT = 'OUTPUT'
+    
+    # Logical operators
+    AND = 'AND'
+    OR = 'OR'
+    NOT = 'NOT'
+    
+    # Procedure definition and calling
+    PROCEDURE = 'PROCEDURE'
+    ENDPROCEDURE = 'ENDPROCEDURE'
+    CALL = 'CALL'
+    
+    # Making a parameter pass by reference in a procedure
+    BYREF = 'BYREF'
+    
+    # Function definition and returning
+    FUNCTION = 'FUNCTION'
+    RETURNS = 'RETURNS'
+    RETURN = 'RETURN'
+    ENDFUNCTION = 'ENDFUNCTION'
+    
+    # Selection keywords
+    IF = 'IF'
+    THEN = 'THEN'
+    ELSEIF = 'ELSEIF'
+    ENDIF = 'ENDIF'
+    
+    CASE = 'CASE'
+    OTHERWISE = 'OTHERWISE'
+    ENDCASE = 'ENDCASE'
+    
+    ##### Operators
+    MODULUS = 'MOD'
+    INT_DIV = 'DIV'
+    
     PLUS = '+'
     HYPHEN = '-'
     ASTERISK = '*'
     FORWARD_SLASH = '/'
     CARET = '^'
-    MODULUS = 'MOD'
-    INT_DIV = 'DIV'
+    PERIOD = '.'
     
     ASSIGNMENT = '<-'
     COLON = ':'
     
-    # Comments
+    ##### Comments
     SINGLE_LINE_COMMENT = '//'
     
-    # Comparison
+    ##### Comparison
     EQUALS_TO = '='
     NOT_EQUALS_TO = '<>'
     AMPERSAND = '&'
@@ -36,7 +105,7 @@ class TokenTypes(Enum):
     R_ANGLE_BRACKET = '>'
     GREATER_OR_EQUALS_TO = '>='
     
-    # Delimiters
+    ##### Delimiters
     L_PARENTHESES = '('
     R_PARENTHESES = ')'
     
